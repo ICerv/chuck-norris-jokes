cda7c85d711706e58cb72540766e434218b3a32d
Inna Červenková <inna.cervenkova@devix.cz>
2025-01-04 22:27:09 +0100

    feat: add draggable Reset button

---
+++ src/components/ResetButton.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import Draggable from 'react-draggable';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import { Box, IconButton } from '@mui/material';
+import theme from '../theme';
+
+const ResetButton: React.FC<{ onResetClick: () => void }> = ({
+  onResetClick,
+}) => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragStart = () => {
+    setIsDragging(true);
+  };
+
+  const handleDragStop = () => {
+    setIsDragging(false);
+  };
+
+  const handleClick = () => {
+    if (!isDragging) {
+      onResetClick();
+    }
+  };
+
+  return (
+    <Draggable onStart={handleDragStart} onStop={handleDragStop}>
+      <Box
+        sx={{
+          position: 'fixed',
+          bottom: '1rem',
+          right: { xs: '1rem', sm: '2rem' },
+          zIndex: 1000,
+          cursor: 'grab',
+        }}
+      >
+        <IconButton
+          onClick={handleClick}
+          sx={{
+            backgroundColor: theme.palette.secondary.main,
+            color: theme.palette.secondary.contrastText,
+            '&:hover': {
+              color: theme.palette.secondary.main,
+              backgroundColor: 'transparent',
+              border: `2px solid ${theme.palette.secondary.main}`,
+            },
+          }}
+        >
+          <RestartAltIcon fontSize="large" />
+        </IconButton>
+      </Box>
+    </Draggable>
+  );
+};
+
+export default ResetButton;
